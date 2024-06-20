568b75480c015c522bb0d810f38f03a16a5b4620
mgutierrez <mgutierrez6@correo.um.edu.uy>
2024-06-20 18:47:38 -0300

    todo

--- src/spotify/Spotify.java
+++ src/spotify/Spotify.java
@@ -29,30 +29,49 @@ public class Spotify {
     }
 
     public void top10FechaYPais(Date fecha, String pais) {
-        System.out.println("Consulto para la fecha: " + fecha + " y país: " + pais);
 
-        if (fechas.contains(fecha)){
+        int iterations = 10; // Número de iteraciones para calcular el promedio
+        long totalTime = 0;
+        long memoryBefore = getUsedMemory();
 
-            HashTable<String,BinarySearchTree<Integer,Song>> paises = fechas.search(fecha);
-            if (paises != null && paises.contains(pais)){
+        if (fechas.contains(fecha)) {
 
-                BinarySearchTree<Integer,Song> cancionesPais = paises.search(pais);
+            HashTable<String, BinarySearchTree<Integer, Song>> paises = fechas.search(fecha);
+            if (paises != null && paises.contains(pais)) {
+
+                BinarySearchTree<Integer, Song> cancionesPais = paises.search(pais);
                 MyList<Song> topCanciones = cancionesPais.inOrderValue();
 
-                System.out.println("Top 10 canciones en el "+pais+" en la " + fecha+ " es:");
-                for (int i = 0; i<10; i++){
-                    Song cancion = topCanciones.get(i);
-                    System.out.println((i+1) + ". "+ cancion.getName());
+                // Iteraciones para calcular el promedio de tiempo de ejecución
+                for (int i = 0; i < iterations; i++) {
+                    long startIterTime = System.nanoTime();
+                    long endTime = System.nanoTime();
+                    long iterExecutionTime = endTime - startIterTime;
+                    totalTime += iterExecutionTime;
                 }
 
-
+                System.out.println("Top 10 canciones en el " + pais + " en la " + fecha + " es:");
+                for (int i = 0; i < 10; i++) {
+                    Song cancion = topCanciones.get(i);
+                    StringBuilder artistas = new StringBuilder();  //para construir la cadena que contiene los nombres de los artistas.
+                    for (int j = 0; j < cancion.getArtist().size(); j++) {
+                        artistas.append(cancion.getArtist().get(j).getName());
+                        if (j < cancion.getArtist().size() - 1) {
+                            artistas.append(", ");
+                        }
+                    }
+                    System.out.println((i + 1) + ". " + cancion.getName() + " - " + artistas.toString());
+                }
+                long averageTime = totalTime / iterations;  // Cálculo del tiempo promedio de ejecución
+                // Impresión de memoria y tiempo promedio
+                long memoryAfter = getUsedMemory();
+                printMemoryAndTime(memoryBefore, memoryAfter, totalTime, averageTime);
             }else{
-                System.out.println("no se encontraron datos paea el pais" + pais);
+                System.out.println("no se encontraron datos para el pais" + pais);
             }
-        }else{
-            System.out.println("no se encontraton datos para la fecha: "+fecha);
+        } else {
+            System.out.println("no se encontraton datos para la fecha: " + fecha);
         }
-
     }
 
     public void top50CanRep(Date fecha2){
@@ -72,7 +91,6 @@ public class Spotify {
                     }
                 }
             }
-
         }
         BinarySearchTree<Integer,String> ordenados= new BinarySearchTreeImpl<>();
         for (int i = 0; i < (cancionesRepe.size() - 1); i++) {
@@ -82,8 +100,10 @@ public class Spotify {
             }
         }
         MyList<String> lista = ordenados.inOrderValue();
+        int contador = 0;
         for (int i = (lista.size() - 1); i > (lista.size() - 6); i--) {
-            System.out.println(lista.get(i));
+            contador++;
+            System.out.println((contador) + ") " + lista.get(i));
         }
     }
 
@@ -123,7 +143,6 @@ public class Spotify {
                 ordenados3.insert(nodo.getValue(), nodo.getKey());
             }
         }
-
         MyList<String> lista3 = ordenados3.inOrderValue();
         for (int i = (lista3.size()-1); i > (lista3.size()-8); i--) {
             System.out.println(lista3.get(i));
@@ -160,49 +179,111 @@ public class Spotify {
                             }
                         }
                     }
-
                 }
-            } else {
-                System.out.println("no se encontraton datos para la fecha: " + fecha);
             }
+        }else {
+            System.out.println("no se encontraton datos para la fecha: " + fecha);
         }
         return veces;
     }
 
+    public int contarCancionesTempo(double tempo1, double tempo2,Date fechaInicio, Date fechaFin){
+        int cantidad = 0;
+
+        if (tempo1>tempo2){
+            double tempo = tempo1;
+            tempo1 = tempo2;
+            tempo2 = tempo;
+
+        }
+
+        boolean paso=false;
+        if(tempo2!=0) {
+            for (int i = 0; i < fechas.size(); i++) {
+                if (fechas.get(i) != null) {
+                    Date fechaActual = fechas.get(i).getKey();
+                    if (fechaActual.compareTo(fechaInicio) >= 0 && fechaActual.compareTo(fechaFin) <= 0) {
+                        HashTable<String, BinarySearchTree<Integer, Song>> paises = fechas.search(fechaActual);
+                        if (paises != null) {
+                            for (int j = 0; j < paises.size(); j++) {
+                                NodeHash<String, BinarySearchTree<Integer, Song>> nodoPais = paises.get(j);
+                                if (nodoPais != null) {
+                                    BinarySearchTree<Integer, Song> cancionesPais = nodoPais.getValue();
+                                    if (cancionesPais != null) {
+                                        MyList<Song> canciones = cancionesPais.inOrderValue();
+                                        for (int k = 0; k < canciones.size(); k++) {
+                                            Song cancion = canciones.get(k);
+                                            double tempo = cancion.getTempo();
+                                            if (tempo >= tempo1 && tempo <= tempo2) {
+                                                cantidad++;
+                                                paso = true;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (!paso){
+                System.out.println("No se encontraron datos para las fechas dadas");
+            }
+        }else{
+            System.out.println("No se encontraron datos para las fechas dadas");
+        }
+
+        return cantidad;
+    }
+
     void menu(Spotify spoti){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el numero de su consulta:");
+        System.out.println("Ingrese el numero de su consulta: \n  1. Top 10 canciones en una fecha y país a tu eleccion. \n  2. Top 5 canciones que aparecen en más top 50 en un día a tu eleccion. \n  3. Top 7 artistas que más aparecen en los top 50 para un rango de fechas a tu eleccion. \n" +
+                "  4. Cantidad de veces que aparece un artista específico en un top 50 en una fecha a tu eleccion. \n  5. Cantidad de canciones con un tempo en un rango específico para un rango específico de fechas a tu eleccion.");
         int opcion = sc.nextInt();
         sc.nextLine();
         switch (opcion) {
 
             case 1:
+                System.out.println("Ingrese la fecha en la que quiere consultar (YYYY-MM-DD):");
                 Date fecha1 = spoti.askFecha(sc);
                 String pais = spoti.askPais(sc);
                 spoti.top10FechaYPais(fecha1, pais);
                 volver(sc,spoti);
 
             case 2:
+                System.out.println("Ingrese la fecha en la que quiere consultar (YYYY-MM-DD):");
                 Date fecha2 = spoti.askFecha(sc);
-
                 spoti.top50CanRep(fecha2);
-
                 volver(sc,spoti);
 
             case 3:
+                System.out.println("Ingrese la fecha de inicio del rango que quiere consultar (YYYY-MM-DD):");
                 Date fecha3inicio = spoti.askFecha(sc);
+                System.out.println("Ingrese la fecha de fin del rango que quiere consultar (YYYY-MM-DD):");
                 Date fecha3fin = spoti.askFecha(sc);
-
                 spoti.top7Artistas(fecha3inicio,fecha3fin);
-
                 volver(sc, spoti);
 
             case 4:
-                Date fecha3 = spoti.askFecha(sc);
-                System.out.println("Ingrese el nombre del artista:");
-                String artista = sc.nextLine();
-                int veces = spoti.contarArtista(fecha3, artista);
-                System.out.println("El artista " + artista + " aparece " + veces + " veces en el top 50 en la fecha " + fecha3);
+                System.out.println("Ingrese la fecha en la que quiere consultar (YYYY-MM-DD):");
+                Date fecha = spoti.askFecha(sc);
+                String artista = spoti.askArtista(sc);
+                int veces = spoti.contarArtista(fecha, artista);
+                System.out.println("El artista " + artista + " aparece " + veces + " veces en el top 50 en la fecha " + fecha);
+                volver(sc, spoti);
+
+            case 5:
+                System.out.println("Ingrese el tempo inicial del rango que quiere consultar:");
+                double tempo1 = spoti.askTempo(sc);
+                System.out.println("Ingrese el tempo final del rango que quiere consultar:");
+                double tempo2 = spoti.askTempo(sc);
+                System.out.println("Ingrese la fecha de inicio del rango que quiere consultar (YYYY-MM-DD):");
+                Date fechaInicio = spoti.askFecha(sc);
+                System.out.println("Ingrese la fecha de fin del rango que quiere consultar (YYYY-MM-DD):");
+                Date fechaFin = spoti.askFecha(sc);
+                int cantidad = spoti.contarCancionesTempo(tempo1,tempo2,fechaInicio,fechaFin);
+                System.out.println("La cantidad de canciones con un tempo entre "+ tempo1 + " y "+tempo2+ " entre las fechas " +fechaInicio+ " y "+fechaFin+ " es: " + cantidad);
                 volver(sc, spoti);
 
             default:
@@ -211,9 +292,8 @@ public class Spotify {
         }
     }
 
-
     Date askFecha(Scanner sc) {
-        System.out.println("Ingrese la fecha en la que quiere consultar:");
+        //System.out.println("Ingrese la fecha en la que quiere consultar:");
         String fechaSC = sc.nextLine();
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -226,9 +306,7 @@ public class Spotify {
     }
 
     void volver(Scanner sc, Spotify spoti){
-        System.out.println("\nDesea volver a realizar una consulta?");
-        System.out.println("1- Si");
-        System.out.println("2- No");
+        System.out.println("\nDesea volver a realizar una consulta? \n 1- Si \n 2- No");
         int opcion2 = sc.nextInt();
         if(opcion2==1){
             menu(spoti);
@@ -245,6 +323,50 @@ public class Spotify {
             askPais(sc);
         }
         return null;
+    }
+
+    String askArtista(Scanner sc) {
+        System.out.println("Ingrese el artista que quiere consultar:");
+        String artista = sc.nextLine();
+        if (artista != null && !artista.isEmpty()) {
+            return artista;
+        } else {
+            System.out.println("Artista inválido. Inténtelo de nuevo.");
+            askArtista(sc);
+        }
+        return null;
+    }
+
+    double askTempo(Scanner sc){
+        //System.out.println("Ingrese el tempo que quiere consultar:");
+        String tempo = sc.nextLine();
+        if (tempo != null){
+            try{
+                return Double.parseDouble(tempo);
+            }catch (NumberFormatException e){
+                System.out.println("Tempo invalido. Inténtelo de nuevo.");
+                askTempo(sc);
+            }
+        }else {
+            System.out.println("Tempo invalido. Inténtelo de nuevo.");
+            askTempo(sc);
+        }
+        return 0;
+    }
+
+    private static long getUsedMemory() {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        return runtime.totalMemory() - runtime.freeMemory();
+    }
+
+    private static void printMemoryAndTime(long memoryBefore, long memoryAfter, long executionTime, long promedio) {
+        System.out.println("\nMemoria utilizada antes de la consulta: " + memoryBefore + " bytes");
+        System.out.println("Memoria utilizada después de la consulta: " + memoryAfter + " bytes");
+        System.out.println("Memoria consumida por la consulta: " + (memoryAfter - memoryBefore) + " bytes");
+        System.out.println();
+        System.out.println("Tiempo de ejecución total de la consulta: " + executionTime + " nanosegundos");
+        System.out.println("Tiempo de ejecución promedio: " + promedio + " nanosegundos");
     }
 
     public HashTable<Date, HashTable<String, BinarySearchTree<Integer, Song>>> getFechas() {
